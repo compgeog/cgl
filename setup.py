ddaf86095243ef4cf556d148c9a0ede9cbe1e564
Ningchuan Xiao <ncxiao@gmail.com>
2017-10-19 23:12:57 -0400

    init files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name = 'cgl',
+      version = '0.0.1',
+      description = 'Computational Geography Library',
+      author = 'Ningchuan Xiao',
+      author_email = 'ncxiao@gmail.com',
+      url='http://github.com/compgeog/cgl',
+      packages = ['cgl', 'cgl/util']
+      )
