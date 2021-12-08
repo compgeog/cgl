869ba9ff3623d33670a55f643bd80f1e02481ec1
Ningchuan Xiao <ncxiao@gmail.com>
2021-12-08 13:30:38 -0500

    path

--- cgl/__init__.py
+++ cgl/__init__.py
@@ -2,7 +2,7 @@ import cgl
 
 # import some top level class explicitly
 
-from cgl.point import Point
-from cgl.kdtree import kdtree
+from .point import Point
+from .kdtree import kdtree
 
 __version__ = '0.1.0'
